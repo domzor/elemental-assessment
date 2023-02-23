615c62871beb0d6cea8424f3fc501f5d55d58041
Dom <hexagonalbanana@gmail.com>
2023-02-23 21:11:47 +0200

    Simplified link fix function

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,8 @@
-// Smooth Scroll On Links
+// Disable links scrolling to top
 
 $(document).ready(function () {
-  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute("href")).scrollIntoView({
-        behavior: "smooth",
-      });
-    });
+  $('a[href="#"').click(function (e) {
+    e.preventDefault();
   });
 });
 
